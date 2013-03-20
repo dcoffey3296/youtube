98570224800b14137cb32e36d5cf3e48c89fb44a
cs50 <cs50@capture50.(none)>
2013-03-20 15:38:07 -0400

    tryign to figure out db

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "this is the root dir\n" . DATABASE_URL;
-echo "\n" . HEROKU_POSTGRESQL_COBALT_URL;
+echo "this is the root dir\n" . getenv("DATABASE_URL");
+echo "\n" . getenv("HEROKU_POSTGRESQL_COBALT_URL");
 
 ?>
