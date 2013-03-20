391766a150225269068439dadcaa23baaa4d94b8
cs50 <cs50@capture50.(none)>
2013-03-20 15:49:24 -0400

    testing postgress connection

---
+++ common/constants.php
@@ -0,0 +1,5 @@
+<?php
+
+define("DATABASE_INFO", "dbname=d9bojppolrev2q host=ec2-107-22-183-27.compute-1.amazonaws.com port=5432 user=wpjhhgpttdbjkr password=g4agm1SLhgM6MyLe4g09UEFMDo sslmode=require");
+
+?>
--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
+	// getenv("DATABASE_URL");
+	// getenv("HEROKU_POSTGRESQL_COBALT_URL");
+	// getenv("DATABASE");
+// credentials: dbname=d9bojppolrev2q host=ec2-107-22-183-27.compute-1.amazonaws.com port=5432 user=wpjhhgpttdbjkr password=g4agm1SLhgM6MyLe4g09UEFMDo sslmode=require
 
-echo "this is the root dir\n" . getenv("DATABASE_URL");
-echo "\n" . getenv("HEROKU_POSTGRESQL_COBALT_URL");
+require_once(dirname(__FILE__)) . "/common/constants.php";
+
+echo "this is the root dir\n";
+if ($dbconn = pg_connect(DATABASE_INFO))
+{
+	echo "CONNECTED TO POSGRES\n";
+	echo "closeing DB";
+	pg_close($dbconn);
+}
+else
+	echo "CONNECTION FAILED";
+
+
 
 ?>
